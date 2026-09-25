3799136d3bcdee3ef5dc3c982fe99ec617f559c5
Ballast releases <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-25 22:16:47 +0000

    Release Ballast 0.1.0-alpha.2

--- Formula/ballast.rb
+++ Formula/ballast.rb
@@ -1,18 +1,18 @@
 class Ballast < Formula
   desc "Keep your machine responsive while coding agents work"
   homepage "https://github.com/anur4ag/ballast"
-  version "0.1.0-alpha.1"
+  version "0.1.0-alpha.2"
   license any_of: ["MIT", "Apache-2.0"]
   depends_on macos: :big_sur
 
   on_arm do
-    url "https://github.com/anur4ag/ballast/releases/download/v0.1.0-alpha.1/ballast-0.1.0-alpha.1-aarch64-apple-darwin.tar.gz"
-    sha256 "29665b1edaf46d070acdf44f8f8b0bbe45deff07f673512f43dd9131f527d3dc"
+    url "https://github.com/anur4ag/ballast/releases/download/v0.1.0-alpha.2/ballast-0.1.0-alpha.2-aarch64-apple-darwin.tar.gz"
+    sha256 "407874553d72f84f7697bc44de79cdc13a6200ca520b88251e4d3784ed375f53"
   end
 
   on_intel do
-    url "https://github.com/anur4ag/ballast/releases/download/v0.1.0-alpha.1/ballast-0.1.0-alpha.1-x86_64-apple-darwin.tar.gz"
-    sha256 "a6b2df8d247909cc5520d82cdeeee7fff1f254ed89a73f25ac176afd476890f5"
+    url "https://github.com/anur4ag/ballast/releases/download/v0.1.0-alpha.2/ballast-0.1.0-alpha.2-x86_64-apple-darwin.tar.gz"
+    sha256 "18f6bae5a112d4e43480240e18a65fd77f53cfac8d098a6195219b95ee60a5d4"
   end
 
   def install
